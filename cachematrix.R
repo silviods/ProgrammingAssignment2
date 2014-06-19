ce7f43e50b113d614d9bf2e1c4a580bc96796fe3
silviods <silvio.hirashiki@yahoo.ca>
2014-06-19 13:16:28 -0400

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## SEE MY UNIT TEST IN THE END OF THIS CODE
+
 ## Write a short comment describing this function
 ## Objective: declare functions in a list, to:
-## 1 - set the value of the vector
-## 2 - get the value of the vector
-## 3 - set the value of the matrix inverse using solve
-## 4 - get the value of the matrix inverse
+##      1 - set the value of the vector
+##      2 - get the value of the vector
+##      3 - set the value of the matrix inverse using solve
+##      4 - get the value of the matrix inverse
+## This returns a list containing the functions
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -16,9 +19,12 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() x
     setinverse <- function(solve) m <<- solve
     getinverse <- function() m
-    list(set = set, get = get,
+    
+    myfunctionlist <- list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
+    
+    return(myfunctionlist)
 }
 
 ## Write a short comment describing this function
@@ -46,3 +52,17 @@ cacheSolve <- function(x, ...) {
     x$setinverse(matrixinverse)
     matrixinverse
 }
+
+## Unit Test (example)
+## > source("cachematrix.R")
+## > m1 <- matrix(c(2, 4, 3, 1, 5, 7, 7, 4, 2, 6, 5, 9, 2, 4, 8, 9), nrow=4, ncol=4)
+## > m2 <- makeCacheMatrix(m1)
+## > cacheSolve(m2)
+##          [,1]        [,2]       [,3]        [,4]
+## [1,] -1.06842105  0.58421053  0.3894737 -0.36842105
+## [2,]  0.70526316 -0.25263158 -0.1684211  0.10526316
+## [3,]  0.05789474  0.12105263 -0.2526316  0.15789474
+## [4,] -0.25263158 -0.07368421  0.2842105 -0.05263158
+## 
+## if you call cacheSolve a secont time, it will show you message
+## "getting cached data" and then return the inverted matrix
